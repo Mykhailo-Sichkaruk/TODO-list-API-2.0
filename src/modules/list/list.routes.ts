ad5cd081ab060fbd8c1c1f6ab6046ab0e16f1b67
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-08-06 21:28:12 +0200

    Delete list. prefix from file names

--- src/modules/list/list.routes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { createList } from "./list.controller";
-
-async function listRoutes(server: FastifyInstance) {
-	console.log("listRoutes");
-	server.post("/", createList);
-}
-
-export default listRoutes;
