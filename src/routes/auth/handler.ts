b8433b8357ec33d0385f280c02d3a404a986b932
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-08-07 23:33:53 +0200

    Delete try catch sinse we have custom handler

--- src/routes/auth/handler.ts
+++ src/routes/auth/handler.ts
@@ -5,43 +5,34 @@ const prisma = new PrismaClient();
 
 const postRegister = async (request, reply) => {
 	const { login, password } = request.body;
-	try {
-		// Check if user already exists
-		let user = await prisma.user.findUnique({ where: { login } });
-		if (user)
-			return reply.code(400).send({ message: "User already exists" });
+	// Check if user already exists
+	let user = await prisma.user.findUnique({ where: { login } });
+	if (user)
+		return reply.code(400).send({ message: "User already exists" });
 		// Create new user
-		user = await prisma.user.create({
-			data: {
-				login,
-				password: await bcrypt.hash(password, 10),
-			},
-		}) as unknown as User;
-		// Return token and user
-		const token = await reply.jwtSign({ id: user.id });
-		return reply.code(200).send({ token, user });
-	} catch (err) {
-		reply.code(500).send({ message: "Something went wrong on the server" });
-	}
+	user = await prisma.user.create({
+		data: {
+			login,
+			password: await bcrypt.hash(password, 10),
+		},
+	}) as unknown as User;
+	// Return token and user
+	const token = await reply.jwtSign({ id: user.id });
+	return reply.code(200).send({ token, user });
 };
 
 const postLogin = async (request, reply) => {
 	const { login, password } = request.body;
-	try {
-		// Check if user exists
-		const user = await prisma.user.findUnique({ where: { login } });
-		if (!user)
-			return reply.code(400).send({ message: "User doesn't exist" });
+	// Check if user exists
+	const user = await prisma.user.findUnique({ where: { login } });
+	if (!user)
+		return reply.code(400).send({ message: "User doesn't exist" });
 		// Check if password is correct
-		if (!await bcrypt.compare(password, user.password))
-			return reply.code(400).send({ message: "Password is incorrect" });
+	if (!await bcrypt.compare(password, user.password))
+		return reply.code(400).send({ message: "Password is incorrect" });
 		// Return token and user
-		const token = await reply.jwtSign({ id: user.id });
-		return reply.code(200).send({ token, user });
-
-	} catch (err) {
-		reply.code(500).send({ message: "Something went wrong on the server" });
-	}
+	const token = await reply.jwtSign({ id: user.id });
+	return reply.code(200).send({ token, user });
 };
 
 export { postRegister, postLogin };
