faa392c2e23d42982e8cf582e6c29ca79d804862
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-08-07 18:42:35 +0200

    Register new plugins

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
-import Fastify from "fastify";
-import authRoutes from "./modules/auth/index";
+import authRoutes from "./routes/auth/index";
+import listRoutes from "./routes/list/index";
+import isSubscribed from "./plugins/isSubscribed";
+import isListExists from "./plugins/isListExists";
+import prismaPlugin from "./plugins/prisma";
 import jwtPlugin from "./plugins/jwt";
-import listRoutes from "./modules/list/index";
+import Fastify from "fastify";
 
 const fastify = Fastify({ logger: true });
 
 async function main() {
 
 	fastify.register(jwtPlugin);
+	fastify.register(prismaPlugin);
+	fastify.register(isListExists);
+	fastify.register(isSubscribed);
 	fastify.register(authRoutes, { prefix: "/auth" });
 	fastify.register(listRoutes, { prefix: "/list" });
 
