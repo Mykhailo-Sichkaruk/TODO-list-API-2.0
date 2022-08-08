9d1719d13bcce9ad70cc5e9fa03ed69b00edd5da
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-08-08 21:37:25 +0200

    Refactore

--- src/app.ts
+++ src/app.ts
@@ -11,26 +11,22 @@ import Fastify from "fastify";
 
 const fastify = Fastify({ logger: true });
 
-async function main() {
-	await fastify.register(fastifySwagger, fastifySwaggerOptions);
+fastify.register(fastifySwagger, fastifySwaggerOptions);
 
-	fastify.setErrorHandler(errorHandler);
-	await fastify.register(jwtPlugin);
-	await fastify.register(prismaPlugin);
-	await fastify.register(isListExists);
-	await fastify.register(isSubscribed);
-	await fastify.register(authRoutes, { prefix: "/auth" });
-	await fastify.register(listRoutes, { prefix: "/list" });
-	await fastify.ready();
-	fastify.swagger();
+fastify.setErrorHandler(errorHandler);
+fastify.register(jwtPlugin);
+fastify.register(prismaPlugin);
+fastify.register(isListExists);
+fastify.register(isSubscribed);
+fastify.register(authRoutes, { prefix: "/auth" });
+fastify.register(listRoutes, { prefix: "/list" });
+fastify.ready();
+fastify.swagger();
 
-	try {
-		fastify.listen({ port: process.env.PORT as unknown as number, host: "0.0.0.0" });
-		console.log(fastify.printRoutes());
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
+try {
+	fastify.listen({ port: process.env.PORT as unknown as number, host: "0.0.0.0" });
+	console.log(fastify.printRoutes());
+} catch (err) {
+	fastify.log.error(err);
+	process.exit(1);
 }
-
-main();
