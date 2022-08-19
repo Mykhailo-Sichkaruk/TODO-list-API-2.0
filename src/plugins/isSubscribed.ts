59f422b358ae6d24d45be7524be3b99449edc47b
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-08-19 21:54:30 +0200

    Add check for /task rpute

--- src/plugins/isSubscribed.ts
+++ src/plugins/isSubscribed.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance, FastifyReply } from "fastify";
 import { Server } from "http";
-import { Request } from "./prisma";
+import { Request } from "./prisma.js";
 import fp from "fastify-plugin";
 
 declare module "fastify" {
@@ -11,10 +11,18 @@ interface FastifyInstance {
 
 export default fp<Server>(async (fastify: FastifyInstance) => {
 	fastify.decorate("isSubscribed", async (request: Request, reply: FastifyReply) => {
+		let id;
+
+		if (request.url === "/task" && request.method !== "POST") {
+			id = request.task.listId;
+		} else if (request.params.id) {
+			id = request.params.id;
+		}
+
 		const subscriber = await fastify.prisma.list.findFirst({
-			where: { id: request.params.id, subscribers: { some: { id: request.user.id } } },
+			where: { id, subscribers: { some: { id: request.user.id } } },
 			select: { id: true, title: true, authorId: true } });
 		if (!subscriber)
-			reply.code(403).send({ message: "You are not a subscriber of this list" });
+			reply.code(403).send({ message: `You are not a subscriber of list {id:${id}}` });
 	});
 });
