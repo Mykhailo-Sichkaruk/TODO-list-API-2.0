6e4c600c7c9e90d9d593c413d51525c348a23a56
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-08-20 12:15:46 +0200

    Fix bug - no deletion on request

--- src/plugins/isSubscribed.ts
+++ src/plugins/isSubscribed.ts
@@ -13,15 +13,15 @@ export default fp<Server>(async (fastify: FastifyInstance) => {
 	fastify.decorate("isSubscribed", async (request: Request, reply: FastifyReply) => {
 		let id;
 
-		if (request.url === "/task" && request.method !== "POST") {
+		if (request.url.startsWith("/task") && request.method !== "POST") {
 			id = request.task.listId;
 		} else if (request.params.id) {
 			id = request.params.id;
 		}
-
 		const subscriber = await fastify.prisma.list.findFirst({
 			where: { id, subscribers: { some: { id: request.user.id } } },
-			select: { id: true, title: true, authorId: true } });
+			select: { id: true, title: true, authorId: true },
+		});
 		if (!subscriber)
 			reply.code(403).send({ message: `You are not a subscriber of list {id:${id}}` });
 	});
