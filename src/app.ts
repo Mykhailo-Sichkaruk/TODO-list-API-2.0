f87a2711bef54873f990b6f5af7933935891fc51
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-08-20 14:57:16 +0200

    Use @fastify/autoload

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-import authRoutes from "./routes/auth/index.js";
-import listRoutes from "./routes/list/index.js";
 import errorHandler from "./plugins/errorHandler.js";
 import isSubscribed from "./plugins/isSubscribed.js";
 import isListExists from "./plugins/isListExists.js";
@@ -9,28 +7,34 @@ import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerOptions from "./plugins/swaggerDocs.js";
 import { fastify, FastifyInstance } from "fastify";
 import fastifyOptions from "./plugins/fastifyOptions.js";
-import taskRoutes from "./routes/task/index.js";
 import isTaskExists from "./plugins/isTaskExists.js";
+import autoLoad from "@fastify/autoload";
+import { fileURLToPath } from "url";
+import path, { dirname, join } from "path";
 
-const server: FastifyInstance = fastify(fastifyOptions);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const server = fastify(fastifyOptions);
 
 async function main() {
 	server.setErrorHandler(errorHandler);
-	await server.register(fastifySwagger, fastifySwaggerOptions);
-	await server.register(jwtPlugin);
-	await server.register(prismaPlugin);
-	await server.register(isListExists);
-	await server.register(isSubscribed);
-	await server.register(isTaskExists);
-	await server.register(authRoutes, { prefix: "/auth" });
-	await server.register(listRoutes, { prefix: "/list" });
-	await server.register(taskRoutes, { prefix: "/task" });
+	server.register(fastifySwagger, fastifySwaggerOptions);
+	server.register(jwtPlugin);
+	server.register(prismaPlugin);
+	server.register(isListExists);
+	server.register(isSubscribed);
+	server.register(isTaskExists);
+	server.register(autoLoad, {
+		dir: path.join(__dirname, "routes"),
+		forceESM: true,
+	});
 	await server.ready();
-	server.swagger();
 	console.log(server.printRoutes());
+	server.swagger();
 }
 
 main();
+
 try {
 	await server.listen({ port: process.env.PORT as unknown as number, host: "0.0.0.0" });
 } catch (err) {
