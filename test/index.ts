f0e6082a7a1417a9d23a3127ec8c095b5e74265d
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2022-08-25 23:58:01 +0200

    Add TAP - test framework

---
+++ test/index.ts
@@ -0,0 +1,15 @@
+import startServer from "../src/server";
+import fastifyOptions from "../src/plugins/fastifyOptions";
+
+const server = await startServer({ ajv: fastifyOptions.ajv });
+
+async function test() {
+	server.inject({
+		method: "POST",
+		url: "/auth",
+		payload: {
+			login: "admin",
+			password: "admin",
+		},
+	});
+}
